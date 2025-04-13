d749e2180cae7d5e243890b952a432daabe7513c
PurviMurkute <purvimurkute51@gmail.com>
2025-04-13 16:39:55 +0530

    changes made

--- Guessno.js
+++ Guessno.js
@@ -3,22 +3,38 @@ const randomNum = Math.floor(Math.random() * 21);
 const showResult = document.getElementById("Result");
 const correctGuess = document.getElementById("correct-guess");
 const body = document.getElementById("body");
+const displayScore = document.getElementById("score");
+const displayhighScore = document.getElementById("highscore");
+let score = 20;
+let highscore = 0;
 
 function checkGuess(){
     const userEnteredNum = inputBox.value;
     if(userEnteredNum < randomNum){
-        showResult.innerHTML = `<h2>Too Low</h2>`;
+        showResult.innerHTML = `<h4>📉 Too Low</h4>`;
+        score--;
+        displayScore.innerHTML = `${score}`;
     }
     else if(userEnteredNum > randomNum){
-        showResult.innerHTML = `<h2>Too High</h2>`;
+        showResult.innerHTML = `<h4>📈 Too High</h4>`;
+        score--;
+        displayScore.innerHTML = `${score}`;
     }
     else{
-        showResult.innerHTML = `<h2>Correct Guess</h2>`;
+        showResult.innerHTML = `<h4>🥳 Correct Guess</h4>`;
         correctGuess.innerHTML = `<h3>${randomNum}</h3>`
         body.style.backgroundColor = `rgb(108, 208, 108)`;
+        if(score>highscore){
+            highscore = score;
+            displayhighScore.innerHTML = `${highscore}`
+        }
+        displayScore.innerHTML = `${score}`;
     }
 }
 
 function playAgain(){
     window.location.reload();
+    if(highscore>score){
+        displayhighScore = `${highscore}`;
+    }
 }
